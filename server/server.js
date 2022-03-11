831d5a72278ad45d1a1268586d20a8c9b07a27ba
Leithen <leithen113@gmail.com>
2022-03-10 17:16:00 -0700

    Connected DB to apollo server

--- server/server.js
+++ server/server.js
@@ -75,21 +75,35 @@ const { ApolloServer } = require('apollo-server-express');
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { typeDefs, resolvers } = require('./schema');
+const db = require('./config/connection');
+const PORT = process.env.PORT || 3001;
 
 async function startApolloServer(typeDefs, resolvers) {
-  const app = express();
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
 
-  await server.start();
-  server.applyMiddleware({ app });
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    await server.start();
+    server.applyMiddleware({ app });
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+    app.use(express.static(path.join(__dirname, 'public')));
+
+
+    db.once('open', async () => {
+        await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
+        console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 };
 
 startApolloServer(typeDefs, resolvers);
