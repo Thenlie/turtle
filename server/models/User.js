78a0d40850dce161e7e9c18c560f15747c5d3153
Leithen <leithen113@gmail.com>
2022-03-12 11:09:25 -0700

    Added bcrypt hashing to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -15,6 +16,7 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
+        minlength: 5
     },
     createdAt: {
         type: Date,
@@ -22,6 +24,17 @@ const userSchema = new Schema({
         required: true
     },
 });
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    };
+    next();
+})
+
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password)
+}
 
 const User = model('User', userSchema);
 
