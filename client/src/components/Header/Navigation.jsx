1434043fc392ebd87c99929c418d3cb147ef8d18
Brett Howard <howardcompositions@gmail.com>
2022-03-19 14:01:40 -0600

    start dropdown menu

--- client/src/components/Header/Navigation.jsx
+++ client/src/components/Header/Navigation.jsx
@@ -1,14 +1,38 @@
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 const Navigation = () => {
     return (
-        <nav>
+        <NavBar>
             <ul className='flex justify-end'>
                 <li className='list-none m-5 text-xl hover:text-slate-500'><Link to={'/'}>Home</Link></li>
                 <li className='list-none m-5 text-xl hover:text-slate-500'><Link to={'/forms'}>Forms</Link></li>
+                <li className='list-none m-5 text-xl hover:text-slate-500'><NavBar /></li>
             </ul>
-        </nav>
+        </NavBar>
     )
 };
+
+function NavBar(props) {
+    return (
+        <nav className="navbar">
+          <ul className="navbar-nav">{props.children}</ul>
+        </nav>
+      );
+}
+
+function NavItem(props) {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <li className="nav-item">
+        <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+          {props.icon}
+        </a>
+  
+        {open && props.children}
+      </li>
+    );
+}
 
 export default Navigation;
