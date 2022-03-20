2843380d1efdfa7b8638e1cd506515ff69d440c2
Leithen <leithen113@gmail.com>
2022-03-20 10:15:58 -0600

    Moved target word logic from Display to Game

--- client/src/components/Game/Display/Display.jsx
+++ client/src/components/Game/Display/Display.jsx
@@ -1,15 +1,6 @@
-import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
-import dictionary from '../../../utils/dictionary';
 
-const Display = ({guessArr}) => {
-    const [target, setTarget] = useState('');
-    
-    useEffect(() => {
-        const randInt = Math.floor(Math.random() * 2314);
-        setTarget(dictionary[randInt].toUpperCase());
-    }, [])
-
+const Display = ({guessArr, target}) => {
     return (
         <section className='p-4 m-4 w-1/3 text-center bg-slate-100 rounded-md'> 
             <h2 className='font-bold text-lg mb-2'>Display</h2>
--- client/src/components/Game/Game.jsx
+++ client/src/components/Game/Game.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import dictionary from '../../utils/dictionary';
 import UserInput from './UserInput/UserInput';
 import Display from './Display/Display';
 
 const Game = () => {
     const [guessArr, setGuessArr] = useState([]);
+    const [target, setTarget] = useState('');
+    
+    useEffect(() => {
+        const randInt = Math.floor(Math.random() * 2314);
+        setTarget(dictionary[randInt].toUpperCase());
+    }, [])
 
     return (
         <>
-            <Display guessArr={guessArr} />
+            <Display guessArr={guessArr} target={target} />
             <UserInput guessArr={guessArr} setGuessArr={setGuessArr} />
         </>
     )
