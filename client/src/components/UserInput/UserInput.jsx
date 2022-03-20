a622bdfeaa45ed2814888a565a14bcf1fe63a9db
Leithen <leithen113@gmail.com>
2022-03-19 22:05:36 -0600

    Set up initial input JSX

--- client/src/components/UserInput/UserInput.jsx
+++ client/src/components/UserInput/UserInput.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 const UserInput = () => {
+    const [input, setInput] = useState('');
+
+    const handleChange = (evt) => {
+        setInput(evt.target.value.toUpperCase());
+    };
+
     return (
         <section className='p-4 m-4 w-1/3 text-center bg-slate-100 rounded-md'>
             <h2 className='font-bold text-lg mb-2'>Input</h2>
-            <textarea className='block m-auto p-2 text-lg' rows={1} cols={40}></textarea>
+            <input onChange={handleChange} className='block m-auto p-2 text-lg text-center border-2 border-slate-300 rounded-md focus-visible:border-slate-400 outline-none' value={input}></input>
             <button className='w-1/4 mt-3 my-auto p-2 rounded-lg bg-slate-300 hover:bg-slate-400'>Submit</button>
         </section>
     )
