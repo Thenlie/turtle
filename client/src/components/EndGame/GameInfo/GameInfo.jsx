47a507db973daf2dac1ece4f4e6f126f4778d0d6
maxh1231 <maxhumpherys@gmail.com>
2022-04-05 19:25:55 -0600

    refactored html and styles to match mockup

--- client/src/components/EndGame/GameInfo/GameInfo.jsx
+++ client/src/components/EndGame/GameInfo/GameInfo.jsx
@@ -5,23 +5,36 @@ const GameInfo = () => {
     console.log(location);
 
     return (
-        <>
-            <div>
-                <h3>{location.state.target}</h3>
+        <div className='flex flex-col items-center '>
+            <div className='flex flex-col bg-gray-100 w-1/2 items-center'>
+                <div className='flex flex-col'>
+                    <div className='flex justify-center'>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>G</div>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>R</div>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>E</div>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>A</div>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>T</div>
+                    </div>
+                    <div className='flex justify-center'>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>J</div>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>O</div>
+                        <div className='m-2 p-4 text-4xl bg-gray-300 rounded-lg'>B</div>
+                    </div>
+                </div>
+                <div>
+                    <h3>Your Guesses:</h3>
+                    {location.state.guessArr.length > 0 && (
+                        location.state.guessArr.map((item, i) => (
+                            <p key={i}>{item}</p>
+                        ))
+                    )}
+                </div>
+                <div>
+                    <h3>Score:</h3>
+                    <p>{location.state.guessArr.length}</p>
+                </div>
             </div>
-            <div>
-                <h3>Your Guesses:</h3>
-                {location.state.guessArr.length > 0 && (
-                    location.state.guessArr.map((item, i) => (
-                        <p key={i}>{item}</p>
-                    ))
-                )}
-            </div>
-            <div>
-                <h3>Score:</h3>
-                <p>{location.state.guessArr.length}</p>
-            </div>
-        </>
+        </div>
     );
 };
 
