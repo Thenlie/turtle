5d07261e264fadc7de39975f3badc576d54bcd7d
Leithen <leithen113@gmail.com>
2022-04-06 23:26:34 -0600

    Created formatAge helper function

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,13 +1,17 @@
 import { DateTime } from 'luxon';
 
 export const formatAge = (time) => {
-    // get createdAt
-    // compare to now
-    // conditionally return based on age
-    return time;
+    let y, m, d;
+    const now = DateTime.now();
+    const start = DateTime.fromMillis(parseInt(time));
+    let diff = now.diff(start, ['years', 'months', 'days']).toObject();
+    diff.years > 0 ? y = Math.floor(diff.years) : y = 2;
+    diff.months > 0 ? m = Math.floor(diff.months) : m = 1;
+    diff.days > 1 ? d = Math.floor(diff.days) : d = 1;
+    return (`${y > 0 ? y + ` year${y > 1 ? 's,' : ','}` : ''} ${m > 0 ? m + ` month${m > 1 ? 's, ' : ', '}` : ''}` + d + ` day${d > 1 ? 's' : ''}`);
 };
 
 export const formatLastActive = (time) => {
-    let d = DateTime.fromMillis(parseInt(time));
-    return d.toLocaleString(DateTime.DATE_FULL);
+    let date = DateTime.fromMillis(parseInt(time));
+    return date.toLocaleString(DateTime.DATE_FULL);
 };
