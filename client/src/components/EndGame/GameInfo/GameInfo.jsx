375999838a0073fcc4d4f9ee935c3cae2cd0cc81
maxh1231 <maxhumpherys@gmail.com>
2022-04-06 19:49:24 -0600

    useMutation on endgame

--- client/src/components/EndGame/GameInfo/GameInfo.jsx
+++ client/src/components/EndGame/GameInfo/GameInfo.jsx
@@ -1,9 +1,20 @@
 import { useLocation } from 'react-router-dom'
+import { useMutation } from '@apollo/client';
+import { ADD_SCORE } from '../../../utils/mutations';
+import { useEffect } from 'react';
 
 const GameInfo = ({ user }) => {
-    console.log(user);
+    const [addScore] = useMutation(ADD_SCORE)
     const location = useLocation();
+
+    useEffect(() => {
+        addScore({
+            variables: { userID: user, guesses: location.state.guessArr.length }
+        })
+    }, [])
+
     console.log(location);
+    console.log(user);
 
     return (
         <div className='flex flex-col items-center '>
