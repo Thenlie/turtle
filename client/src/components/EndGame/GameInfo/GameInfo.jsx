2a09bca74d13ee1a714c32f167ccd4e37b6ae5f4
maxh1231 <maxhumpherys@gmail.com>
2022-04-06 22:12:51 -0600

    moved around styles

--- client/src/components/EndGame/GameInfo/GameInfo.jsx
+++ client/src/components/EndGame/GameInfo/GameInfo.jsx
@@ -17,37 +17,32 @@ const GameInfo = ({ user }) => {
     console.log(user);
 
     return (
-        <div className='flex flex-col items-center '>
-            <div className='flex flex-col bg-gray-100 w-1/2 items-center'>
-
-                <div className='flex flex-col justify-center items-center bg-gray-400 w-3/4 h-full rounded-lg'>
-                    <div>
-                        {location.state.guessArr.length > 3 ? (
-                            <ul>
-                                <li>{location.state.guessArr.length - 2}</li>
-                                <li>{location.state.guessArr.length - 1}</li>
-                                <li>{location.state.guessArr.length}</li>
-                                <li>{location.state.guessArr.length + 1}</li>
-                                <li>{location.state.guessArr.length + 2}</li>
-                            </ul>
-                        ) :
-                            <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                            </ul>
-                        }
-                    </div>
-                    <div>
-                        <button className='mx-2'>Home</button>
-                        <button className='mx-2'>Next</button>
-                        <button className='mx-2'>Replay</button>
-                    </div>
-                </div>
+        <div>
+            <div>
+                {location.state.guessArr.length > 3 ? (
+                    <ul>
+                        <li>{location.state.guessArr.length - 2}</li>
+                        <li>{location.state.guessArr.length - 1}</li>
+                        <li>{location.state.guessArr.length}</li>
+                        <li>{location.state.guessArr.length + 1}</li>
+                        <li>{location.state.guessArr.length + 2}</li>
+                    </ul>
+                ) :
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                        <li>5</li>
+                    </ul>
+                }
             </div>
-        </div>
+            <div>
+                <button className='mx-2'>Home</button>
+                < button className='mx-2' > Next</button >
+                <button className='mx-2'>Replay</button>
+            </div >
+        </div >
     );
 };
 
--- client/src/pages/EndGame.js
+++ client/src/pages/EndGame.js
@@ -2,9 +2,15 @@ import { GameInfo, Message } from '../components/EndGame';
 
 const EndGame = ({ user }) => {
     return (
-        <main className='grow'>
-            <GameInfo user={user} />
-            <Message />
+        <main className='grow flex flex-col items-center justify-center'>
+            <div className='p-8 bg-gray-200 rounded-lg '>
+                <div>
+                    <Message />
+                </div>
+                <div className=''>
+                    <GameInfo user={user} />
+                </div>
+            </div>
         </main>
     );
 };
