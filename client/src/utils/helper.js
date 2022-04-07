a181cd921a15f6e8c81a0b8d0989791ce290c34d
Leithen <leithen113@gmail.com>
2022-04-06 23:28:07 -0600

    Small change from testing fixed

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -5,8 +5,8 @@ export const formatAge = (time) => {
     const now = DateTime.now();
     const start = DateTime.fromMillis(parseInt(time));
     let diff = now.diff(start, ['years', 'months', 'days']).toObject();
-    diff.years > 0 ? y = Math.floor(diff.years) : y = 2;
-    diff.months > 0 ? m = Math.floor(diff.months) : m = 1;
+    diff.years > 0 ? y = Math.floor(diff.years) : y = 0;
+    diff.months > 0 ? m = Math.floor(diff.months) : m = 0;
     diff.days > 1 ? d = Math.floor(diff.days) : d = 1;
     return (`${y > 0 ? y + ` year${y > 1 ? 's,' : ','}` : ''} ${m > 0 ? m + ` month${m > 1 ? 's, ' : ', '}` : ''}` + d + ` day${d > 1 ? 's' : ''}`);
 };
