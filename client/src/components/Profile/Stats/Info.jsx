bd38afea4d65908285e3bfa81de3ea4063bea1dd
Leithen <leithen113@gmail.com>
2022-04-11 16:52:05 -0600

    Created Stat Info component structure and initial styling

--- client/src/components/Profile/Stats/Info.jsx
+++ client/src/components/Profile/Stats/Info.jsx
@@ -1,7 +1,16 @@
 const Info = () => {
     return (
-        <div className='bg-slate-100 border border-slate-300 my-2 p-1 shadow-sm'>
-            <p>Info</p>
+        <div className='bg-slate-100 border border-slate-300 my-2 rounded-md shadow-sm'>
+            <h3 className='bg-slate-300 font-bold p-1 rounded-t-md'>Games Played</h3>
+            <div className='p-1'>
+                <p>Daily:</p>
+                <p>Continuous:</p>
+            </div>
+            <h3 className='bg-slate-300 font-bold p-1 rounded-t-md'>Total Guesses</h3>
+            <div className='p-1'>
+                <p>Daily:</p>
+                <p>Continuous:</p>
+            </div>
         </div>
     );
 };
