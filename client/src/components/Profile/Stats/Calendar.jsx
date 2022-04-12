4e0630dfee7ded3dced53c981bab9cb4ff068576
Leithen <leithen113@gmail.com>
2022-04-11 20:00:43 -0600

    Rendering basic react-calendar to Stat Calendar component

--- client/src/components/Profile/Stats/Calendar.jsx
+++ client/src/components/Profile/Stats/Calendar.jsx
@@ -1,7 +1,14 @@
+import React, { useState } from 'react';
+import { Calendar as ReactCalendar } from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 const Calendar = () => {
+    const [value, onChange] = useState(new Date());
+
     return (
-        <div className='bg-slate-100 border border-slate-300 my-2 p-1 shadow-sm'>
-            <p>Calendar</p>
+        <div className='bg-slate-100 border border-slate-300 my-2 rounded-md shadow-sm'>
+            <h3 className='bg-slate-300 font-bold p-1 rounded-t-md'>Calendar</h3>
+            <ReactCalendar onChange={onChange} value={value} className='!w-full !bg-slate-100 !border-none !rounded-b-md'/>
         </div>
     );
 };
