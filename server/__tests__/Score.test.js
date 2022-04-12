6030530d8732f130be1d345d8018adb46b057d52
Leithen <leithen113@gmail.com>
2022-04-11 23:57:09 -0600

    Completed remaining score tests

--- server/__tests__/Score.test.js
+++ server/__tests__/Score.test.js
@@ -6,4 +6,32 @@ test('fake score has userId provided by parameters', async () => {
     expect(score).toBeInstanceOf(ScoreObj);
     expect(score.userID).toBeDefined();
     expect(score.userID).toBe('1234');
-})
+});
+
+test('fake score has valid game type', async () => {
+    const score = await getFakeScore('1234');
+    expect(score).toBeInstanceOf(ScoreObj);
+    expect(score.type).toBeDefined();
+    expect(['cont', 'daily']).toContain(score.type);
+});
+
+test('fake score has valid number of guesses', async () => {
+    const score = await getFakeScore('1234');
+    expect(score).toBeInstanceOf(ScoreObj);
+    expect(score.guesses).toBeDefined();
+    expect(typeof(score.guesses)).toBe('number');
+});
+
+test('fake score has valid word', async () => {
+    const score = await getFakeScore('1234');
+    expect(score).toBeInstanceOf(ScoreObj);
+    expect(score.word).toBeDefined();
+    expect(score.word.length).toEqual(5);
+});
+
+test('fake score has valid createdAt date', async () => {
+    const score = await getFakeScore('1234');
+    expect(score).toBeInstanceOf(ScoreObj);
+    expect(score.createdAt).toBeDefined();
+    expect(score.createdAt).toBeInstanceOf(Date);
+});
