2eef89af41865405a123efd91a48d8f09963062d
maxh1231 <maxhumpherys@gmail.com>
2022-04-13 22:09:25 -0600

    if currentGuesses is greater than 4, appropriate graph data is rendered

--- client/src/components/Charts/EndGameChart/EndGameChart.jsx
+++ client/src/components/Charts/EndGameChart/EndGameChart.jsx
@@ -26,37 +26,22 @@ const EndGameChart = ({ data }) => {
 
     let labelArr = [];
     let dataArr = [];
-    let indexOne = [];
-    let indexTwo = [];
-    let indexThree = [];
-    let indexFour = []
-    let indexFive = [];
 
-    // useEffect(() => {
     if (currentGuesses > 4) {
         labelArr = [currentGuesses - 2, currentGuesses - 1, currentGuesses, currentGuesses + 1, currentGuesses + 2];
-        // dataArr = data.scoresByUser.filter((item) => item.guesses <= location.state.guessArr.length + 2 && item.guesses >= location.state.guessArr.length + -2)
-        indexOne = data.scoresByUser.filter((item) => item.guesses === currentGuesses - 2)
-        indexTwo = data.scoresByuser.filter((item) => item.guesses === currentGuesses - 1)
-        indexThree = data.scoresByuser.filter((item) => item.guesses === currentGuesses)
-        indexFour = data.scoresByuser.filter((item) => item.guesses === currentGuesses + 1)
-        indexFive = data.scoresByuser.filter((item) => item.guesses === currentGuesses + 2)
 
-        dataArr.push(indexOne.length)
-        dataArr.push(indexTwo.length)
-        dataArr.push(indexThree.length)
-        dataArr.push(indexFour.length)
-        dataArr.push(indexFive.length)
+        dataArr.push(data.scoresByUser.filter((item) => item.guesses === currentGuesses - 2).length)
+        dataArr.push(data.scoresByUser.filter((item) => item.guesses === currentGuesses - 1).length)
+        dataArr.push(data.scoresByUser.filter((item) => item.guesses === currentGuesses).length)
+        dataArr.push(data.scoresByUser.filter((item) => item.guesses === currentGuesses + 1).length)
+        dataArr.push(data.scoresByUser.filter((item) => item.guesses === currentGuesses + 2).length)
+
+        console.log(dataArr)
     } else {
         labelArr = [1, 2, 3, 4, 5]
         dataArr = data.scoresByUser.filter((item) => item.guesses >= 1 && item.guesses <= 5)
     }
     console.log(dataArr)
-
-    // }, [data.scoresByUser])
-
-
-
 
     return (
 
@@ -66,7 +51,7 @@ const EndGameChart = ({ data }) => {
                 datasets: [
                     {
                         label: 'Guesses',
-                        data: [4, 1, 1, 1, 56],
+                        data: dataArr,
                         borderColor: 'rgb(255, 99, 132)',
                         backgroundColor: 'rgba(255, 99, 132, 0.5)',
                         fill: true,
