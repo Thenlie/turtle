19dbc0c550b8608488459f09ad38fe333b3c1483
Leithen <leithen113@gmail.com>
2022-04-18 19:14:07 -0600

    Cleaned up UserInput HTML

--- client/src/components/Game/UserInput/UserInput.jsx
+++ client/src/components/Game/UserInput/UserInput.jsx
@@ -61,23 +61,23 @@ const UserInput = ({guessArr, setGuessArr}) => {
                 <p className='text-red-500'>Invalid Word!</p>
             )}
             <div className='flex justify-center'>
-                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>{input.length > 0 ? (input[0]) : ('')}</div>
-                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>{input.length > 1 ? (input[1]) : ('')}</div>
-                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>{input.length > 2 ? (input[2]) : ('')}</div>
-                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>{input.length > 3 ? (input[3]) : ('')}</div>
-                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>{input.length > 4 ? (input[4]) : ('')}</div>
-            </div>
-            <form onSubmit={handleSubmit}>
-                {/* <input id='input' onChange={handleChange} className=' m-auto my-2 p-2 text-lg text-center border-2 border-slate-300 rounded-md focus-visible:border-slate-400 outline-none hidden' value={input}></input> */}
-                <div className='flex justify-between'>
-                    <div className={`w-1/5 my-1 h-2 bg-slate-200 ${input.length > 0 && 'bg-blue-300'}`}></div>
-                    <div className={`w-1/5 my-1 h-2 bg-slate-200 ${input.length > 1 && 'bg-blue-300'}`}></div>
-                    <div className={`w-1/5 my-1 h-2 bg-slate-200 ${input.length > 2 && 'bg-blue-300'}`}></div>
-                    <div className={`w-1/5 my-1 h-2 bg-slate-200 ${input.length > 3 && 'bg-blue-300'}`}></div>
-                    <div className={`w-1/5 my-1 h-2 bg-slate-200 ${input.length > 4 && 'bg-blue-300'}`}></div>
+                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>
+                    {input.length > 0 ? (input[0]) : ('')}
                 </div>
-                <button type='submit' className='w-1/4 my-3 mx-auto p-2 rounded-lg bg-slate-300 hover:bg-slate-400'>Submit</button>
-            </form>
+                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>
+                    {input.length > 1 ? (input[1]) : ('')}
+                </div>
+                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>
+                    {input.length > 2 ? (input[2]) : ('')}
+                </div>
+                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>
+                    {input.length > 3 ? (input[3]) : ('')}
+                </div>
+                <div className='w-20 h-20 m-1 pt-4 bg-white text-3xl border-2 border-slate-300 rounded-md shadow-md'>
+                    {input.length > 4 ? (input[4]) : ('')}
+                </div>
+            </div>
+            <button type='submit' className='w-1/4 my-3 mx-auto p-2 rounded-lg bg-slate-300 hover:bg-slate-400'>Submit</button>
         </>
     );
 };
