902253d958d3f5958cbcf52b74e2485ba45bf665
maxh1231 <maxhumpherys@gmail.com>
2022-04-18 18:54:30 -0600

    added various margins and paddings, added global Exo font

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,24 +13,22 @@ const Home = ({ user }) => {
     };
 
     return (
-        <main className='grow flex flex-col justify-center'>
-            <div className='flex flex-wrap justify-center'>
-                <div className='flex flex-col items-center '>
-                    <div className='flex flex-row text-4xl'>
-                        <div className='bg-[#86CF84] m-2 p-4 rounded'>T</div>
-                        <div className='bg-[#F4F59F] m-2 p-4 rounded'>U</div>
-                        <div className='bg-[#C4C4C4] m-2 p-4 rounded'>R</div>
-                        <div className='bg-[#86CF84] m-2 p-4 rounded'>T</div>
-                        <div className='bg-[#F4F59F] m-2 p-4 rounded'>L</div>
-                        <div className='bg-[#C4C4C4] m-2 p-4 rounded'>E</div>
+        <main className='grow flex flex-col justify-center items-center'>
+            <div className='flex flex-col items-center'>
+                <div className='flex flex-row text-4xl my-8'>
+                    <div className='bg-[#86CF84] m-2 p-4 rounded'>T</div>
+                    <div className='bg-[#F4F59F] m-2 p-4 rounded'>U</div>
+                    <div className='bg-[#C4C4C4] m-2 p-4 rounded'>R</div>
+                    <div className='bg-[#86CF84] m-2 p-4 rounded'>T</div>
+                    <div className='bg-[#F4F59F] m-2 p-4 rounded'>L</div>
+                    <div className='bg-[#C4C4C4] m-2 p-4 rounded'>E</div>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <div className='mb-8'>
+                        <button className='text-2xl bg-[#C4C4C4] rounded-lg py-2 px-6'><Link to="/signup" className='text-black'>Get Started</Link></button>
                     </div>
-                    <div className='flex flex-col items-center'>
-                        <div>
-                            <button className='text-2xl bg-[#C4C4C4] rounded-lg py-2 px-6'><Link to="/signup" className='text-black'>Get Started</Link></button>
-                        </div>
-                        <div>
-                            <span>Already have an account?<Link to="/login" className='text-[#86CF84]'> Login</Link></span>
-                        </div>
+                    <div>
+                        <span>Already have an account?<Link to="/login" className='text-[#86CF84]'> Login</Link></span>
                     </div>
                 </div>
             </div>
