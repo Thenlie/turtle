4eac78d7475a16beda281aadc562c03e60712863
Leithen <leithen113@gmail.com>
2022-04-21 23:03:24 -0600

    Added return home button to lockout component

--- client/src/components/LockOut/LockOut.jsx
+++ client/src/components/LockOut/LockOut.jsx
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LockOut = () => {
     const [time, setTime] = useState({});
@@ -33,15 +34,16 @@ const LockOut = () => {
     }, []);
 
     return (
-        <main>
+        <main className='text-center'>
             <h1 className='text-center font-bold my-4 text-3xl'>Locked Out!</h1>
-            <div className='flex justify-center'>
+            <div className='flex justify-center my-4'>
                 <div className='text-2xl w-14 h-14 text-center bg-[#86CF84] m-2 pt-3 px-4 rounded shadow-lg rotate-[345deg]'>{time.hours}</div>
                 <p className='py-2 text-3xl'>:</p>
                 <div className='text-2xl w-14 h-14 text-center bg-[#C4C4C4] m-2 pt-3 px-4 rounded shadow-lg'>{time.minutes > 9 ? (time.minutes) : ('0' + time.minutes)}</div>
                 <p className='py-2 text-3xl self-end'>.</p>
                 <div className='text-2xl w-14 h-14 text-center bg-[#F4F59F] m-2 pt-3 px-4 rounded shadow-lg'>{time.seconds > 9 ? (time.seconds) : ('0' + time.seconds)}</div>
             </div>
+            <Link to={'/'} className='text-black'><button className='text-2xl bg-[#C4C4C4] hover:bg-slate-300 rounded-lg py-2 px-6'>Return Home</button></Link>
         </main>
     );
 };
