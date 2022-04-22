dd625b811ed6b5e2098f32f8f3d77a8fd6ca0ec9
maxh1231 <maxhumpherys@gmail.com>
2022-04-21 19:33:14 -0600

    paddings and letter G rotation

--- client/src/components/EndGame/Message/Message.jsx
+++ client/src/components/EndGame/Message/Message.jsx
@@ -3,16 +3,16 @@ const Message = () => {
         <div>
             <div className='flex flex-col'>
                 <div className='flex justify-center'>
-                    <div className='m-2 p-4 text-4xl bg-[#86CF84] rounded-lg'>G</div>
-                    <div className='m-2 p-4 text-4xl bg-[#C4C4C4] rounded-lg'>R</div>
-                    <div className='m-2 p-4 text-4xl bg-[#86CF84] rounded-lg'>E</div>
-                    <div className='m-2 p-4 text-4xl bg-[#F4F59F] rounded-lg'>A</div>
-                    <div className='m-2 p-4 text-4xl bg-[#86CF84] rounded-lg'>T</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#86CF84] rounded-lg rotate-[345deg]'>G</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#C4C4C4] rounded-lg'>R</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#86CF84] rounded-lg'>E</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#F4F59F] rounded-lg'>A</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#86CF84] rounded-lg'>T</div>
                 </div>
                 <div className='flex justify-center'>
-                    <div className='m-2 p-4 text-4xl bg-[#F4F59F] rounded-lg'>J</div>
-                    <div className='m-2 p-4 text-4xl bg-[#C4C4C4] rounded-lg'>O</div>
-                    <div className='m-2 p-4 text-4xl bg-[#86CF84] rounded-lg'>B</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#F4F59F] rounded-lg'>J</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#C4C4C4] rounded-lg'>O</div>
+                    <div className='m-2 py-2 px-4 text-4xl bg-[#86CF84] rounded-lg'>B</div>
                 </div>
             </div>
         </div>
